41926f5dfb57297cf2d71538f83503f8f4ceaced
sylvernus akubo <sylvernusakubo@yahoo.com>
2020-04-20 00:45:04 +0100

    Movie card (#23)
    * movie item full height
    
    * dark background-color for better color contrast
    
    * removed extra debug node_modules
    
    * Scroll functionality
    
    * animated the scroll-up button

---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+
+class ScrollButton extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            intervalId: 0
+        };
+    }
+
+    scrollStep() {
+        if (window.pageYOffset === 0) {
+            clearInterval(this.state.intervalId);
+        }
+        window.scroll(0, window.pageYOffset - this.props.scrollStepInPx);
+    }
+
+    scrollToTop() {
+        let intervalId = setInterval(this.scrollStep.bind(this), this.props.delayInMs);
+        this.setState({
+            intervalId: intervalId
+        });
+    }
+
+    render() {
+        return <button title='Back to top' className='scroll' onClick={ () => {this.scrollToTop(); }}>
+            <span className='arrow-up glyphicon glyphicon-chevron-up'> </span> 
+            </button>;
+    }
+}
+
+export default ScrollButton;
