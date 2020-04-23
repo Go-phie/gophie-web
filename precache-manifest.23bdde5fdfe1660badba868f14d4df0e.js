ebb0e153caf49cf753c80adf2bcb0448af9f12fb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-23 02:25:40 +0000

    Deploy Go-phie/gophie-web to github.com/Go-phie/gophie-web.git:gh-pages

---
+++ precache-manifest.23bdde5fdfe1660badba868f14d4df0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea075d67ca5176116aab",
+    "url": "/gophie-web/static/js/runtime~main.ea075d67.js"
+  },
+  {
+    "revision": "ce2551ef15703f43baf7",
+    "url": "/gophie-web/static/js/main.ce2551ef.chunk.js"
+  },
+  {
+    "revision": "0bdaa240bca53060b0bb",
+    "url": "/gophie-web/static/js/2.0bdaa240.chunk.js"
+  },
+  {
+    "revision": "ce2551ef15703f43baf7",
+    "url": "/gophie-web/static/css/main.36649015.chunk.css"
+  },
+  {
+    "revision": "0bdaa240bca53060b0bb",
+    "url": "/gophie-web/static/css/2.90034b94.chunk.css"
+  },
+  {
+    "revision": "20f8b47c618a8d198c7d7404ed93160c",
+    "url": "/gophie-web/index.html"
+  }
+];
