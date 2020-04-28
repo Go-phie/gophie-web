a2ea825b8e90a99fa4580fe39367b326a873f77f
silvareal <sylvernusakubo@yahoo.com>
2020-04-28 11:59:37 +0100

    error boundary fixed

--- src/components/pollsAlert.js
+++ src/components/pollsAlert.js
@@ -2,20 +2,20 @@ import React, { useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 import ErrorBoundary from '../utils/error-boundaries';
 
-const PollsAlert = () => {
+function PollsAlert() {
   const [show, setShow] = useState(true);
 
-  if (show) {
     return (
-      <Alert className="gophie-alert" variant="danger" onClose={() => setShow(false)} dismissible>
+      <>
+      <Alert className="gophie-alert" show={show} variant="danger" onClose={() => setShow(false)} dismissible>
         <p>
           Hi, Hope you are enjoying your stay on Gophie,
           We need your feedback about <b>features</b> you will love to see on Gophie.
           <a href="https://pollev.com/devendexter353" target="_blank" rel="noopener noreferrer"> Give Feedback</a>
         </p>
       </Alert>
-    );
-  }
+      </>
+  );
 }
 
 export default function PollsAlertWithErrorBoundary(props) {
