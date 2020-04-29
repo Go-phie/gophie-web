9bb5d2392e23498dce0dc0ea3ba131f5e2a570d5
SLKhadeeja <43624344+SLKhadeeja@users.noreply.github.com>
2020-04-29 14:18:12 +0100

    Stop button implementation for streaming (#63)

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -92,6 +92,11 @@ class Popup extends Component {
     this.setState({ play: true });
   }
 
+  handleStopRequest(e) {
+    e.preventDefault()
+    this.setState({ play: false });
+  }
+
   render() {
     return (
       <Modal
@@ -115,6 +120,15 @@ class Popup extends Component {
             />
 
             {/* Video Stream Play Icon */}
+            {
+            this.state.play?
+            <a
+            id="stop-video"
+            className= "video-stop-button"
+            href="/"
+            onClick={this.handleStopRequest.bind(this)}>
+              <span></span>{" "}  
+            </a>:
             <a
               id="play-video"
               className="video-play-button"
@@ -123,6 +137,7 @@ class Popup extends Component {
             >
               <span> </span>{" "}
             </a>
+  }
             {/* Video Stream Play Icon */}
 
           </section>
