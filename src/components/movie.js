f936f9718fc913b2ed88505b318b1940eef905fb
SLKhadeeja <43624344+SLKhadeeja@users.noreply.github.com>
2020-05-07 22:35:34 +0100

    Relocating average stars for visibility (#72)

--- src/components/movie.js
+++ src/components/movie.js
@@ -3,15 +3,17 @@ import React, { Component } from "react";
 import axios from "axios";
 import { DownloadIcon } from "./icons";
 import { isImageURL, greekFromEnglish, API_ENDPOINTS } from "../utils";
+import Rating from "material-ui-rating";
+import "../css/Popup.css";
 
 export default class Movie extends Component {
   constructor(props) {
     super(props);
     this.state = {
       ratings_api: API_ENDPOINTS.ocena,
+      ratings: {},
     };
   }
-
 
   // Add download to API to make it trackable
   addDownload = () => {
@@ -29,6 +31,31 @@ export default class Movie extends Component {
       .then(() => {
         console.log(`added ${this.props.data.Title} to downloads on ocena`)
       })
+  }
+
+  getAverage = () => {
+    const { data } = this.props;
+    axios
+      .post(this.state.ratings_api + "/movie/ratings/average/", {
+        name: data.Title,
+        engine: data.Source,
+      })
+      .then((res) => {
+        this.setState({
+          ratings: res.data,
+        });
+      })
+      .catch((err) => {
+        if (err) {
+          this.setState({
+            error: true,
+          });
+        }
+      });
+  };
+
+  componentDidMount(){
+    this.getAverage();
   }
 
   render() {
@@ -71,6 +98,43 @@ export default class Movie extends Component {
           <div className="movie__about-meta">
             <p className="movie-source"> {greekFromEnglish(Source)} </p>
             <p className="movie-size"> {Size} </p>
+          </div>
+
+        <div className="rating-summary">
+          <div 
+           className="gophie-modal-rating-container__average"
+           data-tour="my-seventh-step">
+             <Rating
+               value={Math.round(
+                 this.state.ratings.average_ratings
+                   ? this.state.ratings.average_ratings
+                   : 0
+               )}
+               max={5}
+               readOnly={true}
+             />
+           </div>
+
+           <div className="gophie-modal-rating-container__average--container__on-card">
+               <div className="gophie-modal-rating-container__average--container-item-1">
+                 <p>
+                   {this.state.ratings.average_ratings
+                     ? Math.round(
+                         this.state.ratings.average_ratings * 10
+                       ) / 10
+                     : 0}
+                 </p>
+                 <p>/5</p>
+               </div>
+
+               <p className="em-rate">
+                 <span className="em-span">by</span>
+
+                 {this.state.ratings.by
+                   ? Math.round(this.state.ratings.by)
+                   : 0}
+               </p>
+             </div>
           </div>
         </div>
       </div>
