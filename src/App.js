71491c08d5dd8060583d727cd2a0bb772873c544
Diretnan Domnan <diretnandomnan@gmail.com>
2020-05-14 18:24:07 +0100

    Switched to HashRouter for github pages sake

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
+import { HashRouter, Switch, Redirect, Route } from "react-router-dom";
 import Home from './home';
 import React, {Component} from 'react';
 class App extends Component {
@@ -6,13 +6,13 @@ class App extends Component {
  render() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Switch>
       <Route path="/(:engine)" component={Home}/>
       <Route path="/" component={Home}/>
       <Redirect to="/" />
     </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 
  }
