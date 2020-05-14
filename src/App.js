63fa162267e8545864bf94d30cdf3e7ef2e7276f
Diretnan Domnan <diretnandomnan@gmail.com>
2020-05-14 21:02:54 +0100

    Fixed browser navigation issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Switch, Redirect, Route } from "react-router-dom";
+import { Switch, Redirect, Route } from "react-router-dom";
 import Home from './home';
 import React, {Component} from 'react';
 class App extends Component {
@@ -6,13 +6,11 @@ class App extends Component {
  render() {
 
   return (
-    <HashRouter>
     <Switch>
       <Route path="/(:engine)" component={Home}/>
       <Route path="/" component={Home}/>
       <Redirect to="/" />
     </Switch>
-    </HashRouter>
   );
 
  }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 import App from "./App";
+import { HashRouter } from 'react-router-dom';
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toggle/style.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <HashRouter>
+    <App />
+    </HashRouter>
+, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
