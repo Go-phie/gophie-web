2687e8bec6886dd35f970fdbc9f08a3b7481b743
Diretnan Domnan <diretnandomnan@gmail.com>
2020-06-03 11:15:43 +0100

    Route to terms and conditions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import { HashRouter, Switch, Redirect, Route } from "react-router-dom";
-import Home from './home';
-import React, {Component} from 'react';
+import Home from "./home";
+import React, { Component } from "react";
 import Terms from "./terms";
 
 class App extends Component {
-
- render() {
-
-  return (
-    <HashRouter>
-    <Switch>
-      <Route path="/(:engine)" component={Home}/>
-      <Route path="/" component={Home}/>
-      <Route path='/terms' component={Terms} />
-      <Redirect to="/" />
-    </Switch>
-    </HashRouter>
-  );
-
- }
+  render() {
+    return (
+      <HashRouter>
+        <Switch>
+          <Route path="/terms" component={Terms} />
+          <Route path="/(:engine)" component={Home} />
+          <Route path="/" component={Home} />
+          <Redirect to="/" />
+        </Switch>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
