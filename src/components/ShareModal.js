56e4106e8bebb78fb052f66ae09b481fa772b91d
nengakdakup <nengakdakup@gmail.com>
2020-06-12 04:46:07 +0100

    added the share buttons and icons

--- src/components/ShareModal.js
+++ src/components/ShareModal.js
@@ -1,18 +1,60 @@
 import React, { Component } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {
+        FacebookShareButton, FacebookIcon,
+        TwitterShareButton, TwitterIcon,
+        WhatsappShareButton, WhatsappIcon,
+        TelegramShareButton, TelegramIcon,
+        RedditShareButton, RedditIcon} from "react-share"
 
 class ShareModal extends Component {
 
     render(){
-        console.log(this.props.movie);
+        const {movie} = this.props;
+        const url = `https://gophie.cam/shared/${movie.referralID}`;
+        
         
         return(
             <Modal show={this.props.display} onHide={this.props.onHide}>
                 <Modal.Header closeButton>
                     <Modal.Title>Share a Movie</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, youre reading this text in a modal!</Modal.Body>
+                <Modal.Body className="show-grid">
+                    <Container>
+                        <Row>
+                            <Col xs={2} md={1}>
+                                <FacebookShareButton url={url} quote={movie.Title}>
+                                    <FacebookIcon size={32} round={true} />
+                                </FacebookShareButton>
+                            </Col>
+                            <Col xs={2} md={1}>
+                                <TwitterShareButton url={url} title={movie.Title}>
+                                    <TwitterIcon size={32} round={true} />
+                                </TwitterShareButton>
+                            </Col>
+                            <Col xs={2} md={1}>
+                                <WhatsappShareButton url={url} title={movie.Title}>
+                                    <WhatsappIcon size={32} round={true} />
+                                </WhatsappShareButton>
+                            </Col>
+                            <Col xs={2} md={1}>
+                                <TelegramShareButton url={url} title={movie.Title}>
+                                    <TelegramIcon size={32} round={true} />
+                                </TelegramShareButton>
+                            </Col>
+                            <Col xs={2} md={1}>
+                                <RedditShareButton url={url} title={movie.Title}>
+                                    <RedditIcon size={32} round={true} />
+                                </RedditShareButton>
+                            </Col>
+                            
+                        </Row>
+                    </Container>
+                </Modal.Body>
                 <Modal.Footer>
                     <Button variant="danger" onClick={this.props.onHide}>
                         Close
