c59c32e74b7b30dfb46c59ea67b4f29e4be7414c
silvareal <sylvernusakubo@yahoo.com>
2020-07-31 19:20:40 +0100

    App component to AppRoute

---
+++ src/appRoute.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
+import Home from "./pages/home";
+import React from "react";
+import Terms from "./pages/terms";
+import Shared from "./pages/shared";
+
+const AppRoute = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/shared/:referralID" component={Shared} />
+        <Route path="/terms" component={Terms} />
+        <Route path="/(:engine)" component={Home} />
+        <Route path="/" component={Home} />
+        <Route path="/search" component={Home} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoute;
