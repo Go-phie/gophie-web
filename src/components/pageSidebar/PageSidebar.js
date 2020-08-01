05d926fe9c37ef0c29bc718c1596227d1e46f62f
silvareal <sylvernusakubo@yahoo.com>
2020-08-01 11:59:15 +0100

    pathname active link style

--- src/components/pageSidebar/PageSidebar.js
+++ src/components/pageSidebar/PageSidebar.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Style from "./PageSidebar.styles";
 import { GophieLogo } from "../../utils/icons";
-import { greekFromEnglish } from "../../utils";
 
-const PageSidebar = (server) => {
+const PageSidebar = (server, handleServerChange) => {
+
+  let pathname = window.location.pathname;
+  useEffect(() => {
+      pathname = window.location.pathname;
+  }, [window.location.pathname]);
+
   return (
     <Style.Sidebar>
       <Style.Sidebar__Logo>
@@ -11,23 +16,23 @@ const PageSidebar = (server) => {
       </Style.Sidebar__Logo>
       <Style.Sidebar__Nav>
             <Style.Sidebar__NavListItem>
-                <Style.Sidebar__NavLink className={greekFromEnglish(server) === "Server1" ? "active": "" } to={"/Server1"} >Server1</Style.Sidebar__NavLink>
+                <Style.Sidebar__NavLink className={`${pathname.match('/Server1') ? 'active' : ''}`} to={"/Server1"} >Server1</Style.Sidebar__NavLink>
             </Style.Sidebar__NavListItem>
             
             <Style.Sidebar__NavListItem>
-                <Style.Sidebar__NavLink className={greekFromEnglish(server) === "Server2" ? "active": "" } to={"/Server2"} >Server2</Style.Sidebar__NavLink>
+                <Style.Sidebar__NavLink className={`${pathname.match('/Server2') ? 'active' : ''}`} to={"/Server2"} >Server2</Style.Sidebar__NavLink>
             </Style.Sidebar__NavListItem>
 
             <Style.Sidebar__NavListItem>
-                <Style.Sidebar__NavLink className={greekFromEnglish(server) === "Server3" ? "active": "" } to={"/Server3"} >Server3</Style.Sidebar__NavLink>
+                <Style.Sidebar__NavLink className={`${pathname.match('/Server3') ? 'active' : ''}`} to={"/Server3"} >Server3</Style.Sidebar__NavLink>
             </Style.Sidebar__NavListItem>
 
             <Style.Sidebar__NavListItem>
-              <Style.Sidebar__NavLink className={greekFromEnglish(server) === "Server4" ? "active": "" } to={"/Server4"} >Server4</Style.Sidebar__NavLink>
+              <Style.Sidebar__NavLink className={`${pathname.match('/Server4') ? 'active' : ''}`}  to={"/Server4"} >Server4</Style.Sidebar__NavLink>
             </Style.Sidebar__NavListItem>
 
             <Style.Sidebar__NavListItem>
-                <Style.Sidebar__NavLink className={greekFromEnglish(server) === "Server5" ? "active": "" } to={"/Server5"} >Server5</Style.Sidebar__NavLink>
+                <Style.Sidebar__NavLink className={`${pathname.match('/Server5') ? 'active' : ''}`} to={"/Server5"} >Server5</Style.Sidebar__NavLink>
             </Style.Sidebar__NavListItem>
       </Style.Sidebar__Nav>
 
