5c2b40f950212fe668e9e96faef4e5a9e8c21495
silvareal <sylvernusakubo@yahoo.com>
2020-08-01 13:21:45 +0100

    remove useEffect for better performance

--- src/components/pageSidebar/PageSidebar.js
+++ src/components/pageSidebar/PageSidebar.js
@@ -1,39 +1,59 @@
-import React, {useEffect} from "react";
+import React from "react";
 import Style from "./PageSidebar.styles";
 import { GophieLogo } from "../../utils/icons";
 
-const PageSidebar = (server, handleServerChange) => {
-
+const PageSidebar = () => {
   let pathname = window.location.pathname;
-  useEffect(() => {
-      pathname = window.location.pathname;
-  }, [window.location.pathname]);
-
   return (
     <Style.Sidebar>
       <Style.Sidebar__Logo>
         <GophieLogo />
       </Style.Sidebar__Logo>
       <Style.Sidebar__Nav>
-            <Style.Sidebar__NavListItem>
-                <Style.Sidebar__NavLink className={`${pathname.match('/Server1') ? 'active' : ''}`} to={"/Server1"} >Server1</Style.Sidebar__NavLink>
-            </Style.Sidebar__NavListItem>
-            
-            <Style.Sidebar__NavListItem>
-                <Style.Sidebar__NavLink className={`${pathname.match('/Server2') ? 'active' : ''}`} to={"/Server2"} >Server2</Style.Sidebar__NavLink>
-            </Style.Sidebar__NavListItem>
+        <Style.Sidebar__NavListItem>
+          <Style.Sidebar__NavLink
+            className={`${pathname.match("/Server1") ? "active" : ""}`}
+            to={"/Server1"}
+          >
+            Server1 <small>(Movies)</small>
+          </Style.Sidebar__NavLink>
+        </Style.Sidebar__NavListItem>
 
-            <Style.Sidebar__NavListItem>
-                <Style.Sidebar__NavLink className={`${pathname.match('/Server3') ? 'active' : ''}`} to={"/Server3"} >Server3</Style.Sidebar__NavLink>
-            </Style.Sidebar__NavListItem>
+        <Style.Sidebar__NavListItem>
+          <Style.Sidebar__NavLink
+            className={`${pathname.match("/Server2") ? "active" : ""}`}
+            to={"/Server2"}
+          >
+            Server2 <small>(Anime)</small>
+          </Style.Sidebar__NavLink>
+        </Style.Sidebar__NavListItem>
 
-            <Style.Sidebar__NavListItem>
-              <Style.Sidebar__NavLink className={`${pathname.match('/Server4') ? 'active' : ''}`}  to={"/Server4"} >Server4</Style.Sidebar__NavLink>
-            </Style.Sidebar__NavListItem>
+        <Style.Sidebar__NavListItem>
+          <Style.Sidebar__NavLink
+            className={`${pathname.match("/Server3") ? "active" : ""}`}
+            to={"/Server3"}
+          >
+            Server3 <small>(HD)</small>
+          </Style.Sidebar__NavLink>
+        </Style.Sidebar__NavListItem>
 
-            <Style.Sidebar__NavListItem>
-                <Style.Sidebar__NavLink className={`${pathname.match('/Server5') ? 'active' : ''}`} to={"/Server5"} >Server5</Style.Sidebar__NavLink>
-            </Style.Sidebar__NavListItem>
+        <Style.Sidebar__NavListItem>
+          <Style.Sidebar__NavLink
+            className={`${pathname.match("/Server4") ? "active" : ""}`}
+            to={"/Server4"}
+          >
+            Server4 <small>(Series)</small>
+          </Style.Sidebar__NavLink>
+        </Style.Sidebar__NavListItem>
+
+        <Style.Sidebar__NavListItem>
+          <Style.Sidebar__NavLink
+            className={`${pathname.match("/Server5") ? "active" : ""}`}
+            to={"/Server5"}
+          >
+            Server5 <small>(Movies)</small>
+          </Style.Sidebar__NavLink>
+        </Style.Sidebar__NavListItem>
       </Style.Sidebar__Nav>
 
       <div></div>
