bf12d98910ea04d401f80cb1022d868bb8ff22d9
silvareal <sylvernusakubo@yahoo.com>
2020-08-03 15:25:05 +0100

    padding for proper spacing

--- src/components/mobileNav/MobileNavbar.styles.js
+++ src/components/mobileNav/MobileNavbar.styles.js
@@ -8,7 +8,7 @@ const Style = {
         align-items: center;
         justify-content: space-between;
         width: 100%;
-        /* padding: 1.5em; */
+        padding: 1em 1.5em;
         z-index: 999;
         background-color: ${({ theme }) => theme.body};
         display: none;
