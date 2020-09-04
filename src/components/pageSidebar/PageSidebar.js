a2d4681f7086ceded213294ff326fd9b41285fa1
silvareal <sylvernusakubo@yahoo.com>
2020-09-05 00:42:17 +0100

    sidebar overflow for device compatibility

--- src/components/pageSidebar/PageSidebar.js
+++ src/components/pageSidebar/PageSidebar.js
@@ -10,78 +10,83 @@ const PageSidebar = (props) => {
       <Style.Sidebar__Logo>
         <GophieLogo />
       </Style.Sidebar__Logo>
-      <Style.Sidebar__Nav>
-        <Style.Sidebar__NavListItem>
-          <Style.Sidebar__NavLink
-            className={`${pathname.match("/Server1") ? "active" : ""}`}
-            onClick={props.handleServerChange}
-            value="Server1"
-          >
-            Server1 <small>(Movies)</small>
-          </Style.Sidebar__NavLink>
-        </Style.Sidebar__NavListItem>
 
-        <Style.Sidebar__NavListItem>
-          <Style.Sidebar__NavLink
-            className={`${pathname.match("/Server2") ? "active" : ""}`}
-            onClick={props.handleServerChange}
-            value="Server2"
-          >
-            Server2 <small>(Anime)</small>
-          </Style.Sidebar__NavLink>
-        </Style.Sidebar__NavListItem>
+      <Style.Sidebar__Main>
+        <Style.Sidebar__Nav>
+          <Style.Sidebar__NavListItem>
+            <Style.Sidebar__NavLink
+              className={`${pathname.match("/Server1") ? "active" : ""}`}
+              onClick={props.handleServerChange}
+              value="Server1"
+            >
+              Server1 <small>(Movies)</small>
+            </Style.Sidebar__NavLink>
+          </Style.Sidebar__NavListItem>
 
-        <Style.Sidebar__NavListItem>
-          <Style.Sidebar__NavLink
-            className={`${pathname.match("/Server3") ? "active" : ""}`}
-            onClick={props.handleServerChange}
-            value="Server3"
-          >
-            Server3 <small>(HD)</small>
-          </Style.Sidebar__NavLink>
-        </Style.Sidebar__NavListItem>
+          <Style.Sidebar__NavListItem>
+            <Style.Sidebar__NavLink
+              className={`${pathname.match("/Server2") ? "active" : ""}`}
+              onClick={props.handleServerChange}
+              value="Server2"
+            >
+              Server2 <small>(Anime)</small>
+            </Style.Sidebar__NavLink>
+          </Style.Sidebar__NavListItem>
 
-        <Style.Sidebar__NavListItem>
-          <Style.Sidebar__NavLink
-            className={`${pathname.match("/Server4") ? "active" : ""}`}
-            onClick={props.handleServerChange}
-            value="Server4"
-          >
-            Server4 <small>(Series)</small>
-          </Style.Sidebar__NavLink>
-        </Style.Sidebar__NavListItem>
+          <Style.Sidebar__NavListItem>
+            <Style.Sidebar__NavLink
+              className={`${pathname.match("/Server3") ? "active" : ""}`}
+              onClick={props.handleServerChange}
+              value="Server3"
+            >
+              Server3 <small>(HD)</small>
+            </Style.Sidebar__NavLink>
+          </Style.Sidebar__NavListItem>
 
-        <Style.Sidebar__NavListItem>
-          <Style.Sidebar__NavLink
-            className={`${pathname.match("/Server5") ? "active" : ""}`}
-            onClick={props.handleServerChange}
-            value="Server5"
-          >
-            Server5 <small>(Movies)</small>
-          </Style.Sidebar__NavLink>
-        </Style.Sidebar__NavListItem>
+          <Style.Sidebar__NavListItem>
+            <Style.Sidebar__NavLink
+              className={`${pathname.match("/Server4") ? "active" : ""}`}
+              onClick={props.handleServerChange}
+              value="Server4"
+            >
+              Server4 <small>(Series)</small>
+            </Style.Sidebar__NavLink>
+          </Style.Sidebar__NavListItem>
 
-        <Style.Sidebar__NavListItem>
-          <Style.Sidebar__NavLink
-            className={`${pathname.match("/Server6") ? "active" : ""}`}
-            onClick={props.handleServerChange}
-            value="Server6"
-          >
-            Server6 <small>(Korean)</small>
-          </Style.Sidebar__NavLink>
-        </Style.Sidebar__NavListItem>
-      </Style.Sidebar__Nav>
+          <Style.Sidebar__NavListItem>
+            <Style.Sidebar__NavLink
+              className={`${pathname.match("/Server5") ? "active" : ""}`}
+              onClick={props.handleServerChange}
+              value="Server5"
+            >
+              Server5 <small>(Movies)</small>
+            </Style.Sidebar__NavLink>
+          </Style.Sidebar__NavListItem>
 
-      <Style.Sidebar__Footer>
-        <h3>
-          Gophie.cam —{" "}
-          <small>
-            {" "}
-            <Style.Sidebar__FooterLink to={"/terms"}>Terms</Style.Sidebar__FooterLink>
-          </small>
-        </h3>
-        <p>Copyright © Gophie 2020. All Right Reserved</p>
-      </Style.Sidebar__Footer>
+          <Style.Sidebar__NavListItem>
+            <Style.Sidebar__NavLink
+              className={`${pathname.match("/Server6") ? "active" : ""}`}
+              onClick={props.handleServerChange}
+              value="Server6"
+            >
+              Server6 <small>(Korean)</small>
+            </Style.Sidebar__NavLink>
+          </Style.Sidebar__NavListItem>
+        </Style.Sidebar__Nav>
+
+        <Style.Sidebar__Footer>
+          <h3>
+            Gophie.cam —{" "}
+            <small>
+              {" "}
+              <Style.Sidebar__FooterLink to={"/terms"}>
+                Terms
+              </Style.Sidebar__FooterLink>
+            </small>
+          </h3>
+          <p>Copyright © Gophie 2020. All Right Reserved</p>
+        </Style.Sidebar__Footer>
+      </Style.Sidebar__Main>
     </Style.Sidebar>
   );
 };
--- src/components/pageSidebar/PageSidebar.styles.js
+++ src/components/pageSidebar/PageSidebar.styles.js
@@ -1,168 +1,180 @@
-import styled from "styled-components"
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const Style = {
-    // Man sidebar container
-    Sidebar: styled.div`
-        position: fixed;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        width: 230px;
-        height: 100%;
-        overflow: auto;
-        border-right: 2px solid ${({ theme }) => theme.movieBackground};
-        background: ${({ theme }) => theme.body};
+  // Man sidebar container
+  Sidebar: styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    width: 230px;
+    height: 100%;
+    overflow: auto;
+    border-right: 2px solid ${({ theme }) => theme.movieBackground};
+    background: ${({ theme }) => theme.body};
+    display: flex;
+    flex-direction: column;
 
-        @media (max-width: 678px) {
-            left: initial;
-            display: flex;
-            width: 100% !important;
-            align-items: initial;
-            z-index: 999;
-            bottom: initial;
-            height: initial;
-        }
+    @media (max-width: 678px) {
+      left: initial;
+      display: flex;
+      flex-direction: row;
+      overflow: hidden;
+      width: 100% !important;
+      align-items: initial;
+      z-index: 999;
+      bottom: initial;
+      height: initial;
+    }
 
-        @media(max-width: 1141px) {
-            width: 200px;
-        }
-    `,
+    @media (max-width: 1141px) {
+      width: 200px;
+    }
+  `,
 
-    // sidebar logo container
-    Sidebar__Logo: styled.div`
-        padding: 1.5em 0;
-        text-align: left;
-        position: relative;
-        margin: 0 1.5em;
+  // sidebar logo container
+  Sidebar__Logo: styled.div`
+    padding: 1.5em 0;
+    text-align: left;
+    position: relative;
+    margin: 0 1.5em;
 
-        &:before {
-            width: 3em;
-            height: .3em;
-            content: "";
-            display: block;
-            background: ${({ theme }) => theme.yellowDark};
-            border-radius: 1.5em;
-            position: absolute;
-            bottom: 0;
+    &:before {
+      width: 3em;
+      height: 0.3em;
+      content: "";
+      display: block;
+      background: ${({ theme }) => theme.yellowDark};
+      border-radius: 1.5em;
+      position: absolute;
+      bottom: 0;
 
-            @media (max-width: 678px) {
-                width: 2em;
-                height: .2em;
-            }
-        }
+      @media (max-width: 678px) {
+        width: 2em;
+        height: 0.2em;
+      }
+    }
 
-        @media (max-width: 678px) {
-            padding: 1em 0;
-            margin-right: initial;
-        }
-    `,
+    @media (max-width: 678px) {
+      padding: 1em 0;
+      margin-right: initial;
+    }
+  `,
 
-    // Sidebar Navigation
-    Sidebar__Nav: styled.ul`
-        margin: 0 1.5em;
-        margin-top: 5em;
+  Sidebar__Main: styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    position: relative;
+    overflow: auto;
+    justify-content: space-between;
+  `,
 
-        @media (max-width: 678px) {
-            display: flex;
-            margin-top: initial;
-            overflow: scroll hidden;
-            scrollbar-width: none;
-            width: 100%;
-            margin-right: initial;
-            margin-left: initial;
-        }
-    `,
+  // Sidebar Navigation
+  Sidebar__Nav: styled.ul`
+    margin: 0 1.5em;
+    margin-top: 5em;
 
-    Sidebar__NavListItem: styled.li`
-        margin-top: 1em;
-        list-style: none;
+    @media (max-width: 678px) {
+      display: flex;
+      margin-top: initial;
+      overflow: scroll hidden;
+      scrollbar-width: none;
+      width: 100%;
+      margin-right: initial;
+      margin-left: initial;
+    }
+  `,
 
-        @media (max-width: 678px) {
-            white-space: nowrap;
-        }
-    `,
+  Sidebar__NavListItem: styled.li`
+    margin-top: 1em;
+    list-style: none;
 
-    // sidebar Navigation links
-    Sidebar__NavLink: styled.button`
-        color: ${({ theme }) => theme.text};
-        padding: 1em .5em;
-        text-decoration: none;
-        display: block;
-        transition: .3s ease-in;
-        font-weight: 600;
-        outline: none;
-        background: transparent;
-        border-radius: .3em;
-        width: 100%;
-        text-align: left;
+    @media (max-width: 678px) {
+      white-space: nowrap;
+    }
+  `,
 
+  // sidebar Navigation links
+  Sidebar__NavLink: styled.button`
+    color: ${({ theme }) => theme.text};
+    padding: 1em 0.5em;
+    text-decoration: none;
+    display: block;
+    transition: 0.3s ease-in;
+    font-weight: 600;
+    outline: none;
+    background: transparent;
+    border-radius: 0.3em;
+    width: 100%;
+    text-align: left;
 
-        &:hover {
-            color: ${({ theme }) => theme.yellowDark};
-            background: ${({ theme }) => theme.sidebarNav};
-        }
+    &:hover {
+      color: ${({ theme }) => theme.yellowDark};
+      background: ${({ theme }) => theme.sidebarNav};
+    }
 
-        &:active {
-            color: ${({ theme }) => theme.yellowDark};
-            border-radius: .3em;
-            background: ${({ theme }) => theme.sidebarNav};
-        }
+    &:active {
+      color: ${({ theme }) => theme.yellowDark};
+      border-radius: 0.3em;
+      background: ${({ theme }) => theme.sidebarNav};
+    }
 
-        &.active {
-            color: ${({ theme }) => theme.yellowDark};
-            border-radius: .3em;
-            background: ${({ theme }) => theme.sidebarNav};
-        }
+    &.active {
+      color: ${({ theme }) => theme.yellowDark};
+      border-radius: 0.3em;
+      background: ${({ theme }) => theme.sidebarNav};
+    }
 
-        @media (max-width: 678px) {
-            padding: .5em .5em;
-            margin-right: .5rem;
-            font-size: .8em;
-            font-weight: initial;
-        }
+    @media (max-width: 678px) {
+      padding: 0.5em 0.5em;
+      margin-right: 0.5rem;
+      font-size: 0.8em;
+      font-weight: initial;
+    }
 
-        @media (max-width: 375px) {
-            font-size: .7em;
-        }
-    `,
+    @media (max-width: 375px) {
+      font-size: 0.7em;
+    }
+  `,
 
-    Sidebar__Footer: styled.div`
-        display: block;
-        position: absolute;
-        bottom: 0;
-        padding: 18px 22px;
-        width: 100%;
+  Sidebar__Footer: styled.div`
+    display: block;
+    bottom: 0;
+    padding: 18px 22px;
+    margin-top: 2em;
+    width: 100%;
 
-        & > h3 {
-            font-size: .8em;
-            font-weight: 600;
-            color: ${({ theme }) => theme.text};
-        }
+    & > h3 {
+      font-size: 0.8em;
+      font-weight: 600;
+      color: ${({ theme }) => theme.text};
+    }
 
-        & > p {
-            font-size: .65em;
-            opacity: .5;
-            color: ${({ theme }) => theme.text};
-        }
+    & > p {
+      font-size: 0.65em;
+      opacity: 0.5;
+      color: ${({ theme }) => theme.text};
+    }
 
-        @media (max-width: 678px) {
-            display: none;
-        }
-    `,
+    @media (max-width: 678px) {
+      display: none;
+    }
+  `,
 
-    Sidebar__FooterLink: styled(Link)`
-        background: #ede9d9;
-        padding: .27em;
-        background: ${({ theme }) => theme.sidebarNav};
-        color: ${({ theme }) => theme.yellowDark};
-        text-decoration: none;
+  Sidebar__FooterLink: styled(Link)`
+    background: #ede9d9;
+    padding: 0.27em;
+    background: ${({ theme }) => theme.sidebarNav};
+    color: ${({ theme }) => theme.yellowDark};
+    text-decoration: none;
 
-        &.hover {
-            color: ${({ theme }) => theme.text};
-            opacity: .5;
-        }
-    `
-}
+    &.hover {
+      color: ${({ theme }) => theme.text};
+      opacity: 0.5;
+    }
+  `
+};
 
 export default Style;
