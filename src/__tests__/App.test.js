7a187015d54de40f4096ea9c498257e396901863
Diretnan Domnan <diretnandomnan@gmail.com>
2020-09-26 22:39:42 +0100

    fixed tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../App";
+import AppRoute from "../appRoute";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<AppRoute />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
