b8195992f941b2e8b552272aefe655b8087d6f5c
mohamilr <ibrahimdamy7@gmail.com>
2020-10-14 20:44:29 +0100

    Add Installable Feature

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+   apps: [{
+      script: 'server.js',
+      name: 'Gophie-Web',
+   }],
+
+   deploy: {
+      production: {
+         user: 'ubuntu',
+         host: 'ec2-18-130-190-198.eu-west-2.compute.amazonaws.com',
+         key: './gophie-private-key.pem',
+         ref: 'origin/master',
+         ssh_options: ["StrictHostKeyChecking=no", "PasswordAuthentication=no"],
+         repo: 'https://github.com/Go-phie/gophie-web.git',
+         path: '/home/ubuntu/gophie-web',
+         'post-deploy': 'yarn && yarn build && pm2 startOrRestart ecosystem.config.js --env production'
+      }
+   }
+};
