f0fbad045a32d361d7f513e8d1a948e433dcec02
wizzywit <wisdompraise968@gmail.com>
2021-03-25 16:54:17 +0100

    Included the defaultImage and modified music meta tag info

--- src/components/music/Music.js
+++ src/components/music/Music.js
@@ -62,7 +62,7 @@ const MusicGroup = ({
         const url = window.URL.createObjectURL(new Blob([response.data]))
         const link = document.createElement('a')
         link.href = url
-        link.setAttribute('download', `${title}-${artiste}.mp3`)
+        link.setAttribute('download', `${title} - ${artiste}.mp3`)
 
         // Append to html link element page
         document.body.appendChild(link)
