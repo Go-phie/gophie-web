ad79ae08d2a681fde84fce732d941a73699e431b
wizzywit <wisdompraise968@gmail.com>
2021-03-25 16:27:40 +0100

    Player metatag

--- src/components/music/Music.js
+++ src/components/music/Music.js
@@ -30,7 +30,39 @@ const MusicGroup = ({
       navigator.mediaSession.metadata = new window.MediaMetadata({
         title,
         artist: artiste,
-        album: collection
+        album: collection,
+        artwork: [
+          {
+            src: 'https://dummyimage.com/96x96',
+            sizes: '96x96',
+            type: 'image/png'
+          },
+          {
+            src: 'https://dummyimage.com/128x128',
+            sizes: '128x128',
+            type: 'image/png'
+          },
+          {
+            src: 'https://dummyimage.com/192x192',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'https://dummyimage.com/256x256',
+            sizes: '256x256',
+            type: 'image/png'
+          },
+          {
+            src: 'https://dummyimage.com/384x384',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: 'https://dummyimage.com/512x512',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       })
     }
     setCurrentMusic(id)
